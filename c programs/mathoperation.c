d8d53283483487a530148b5cb0db4b20d809db33
Krishna <krishnalkr1210@gmail.com>
2022-01-02 18:54:44 +0530

    update

---
+++ c programs/mathoperation.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b;
+    printf("Enter First Nmmber=");
+    scanf("%d",&a);
+    printf("Enter Second Number=3");
+    scanf("%d",&b);
+    printf("Sum of First number and Second numbers is %d\n",a+b); 
+    printf("Difference of First number and Second numbers is %d\n",a-b); 
+    printf("Product of First number and Second numbers is %d\n",a*b); 
+    printf("Divison of First number and Second numbers is %d\n",a/b); 
+    getchar();
+}
+
+
+
+
