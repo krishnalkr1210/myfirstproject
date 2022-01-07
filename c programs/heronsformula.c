a69c72c72241bc64b86e4f203e52019f4188c10a
Krishna <krishnalkr1210@gmail.com>
2022-01-07 18:21:42 +0530

    change

---
+++ c programs/heronsformula.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+    int a,b,c;
+    float ar;
+    double s;
+    printf("Enter Number");
+    scanf("%d",&s);
+    printf("%d",sqrt(s));
+    
+    getchar();
+    }
+
+    
+    
