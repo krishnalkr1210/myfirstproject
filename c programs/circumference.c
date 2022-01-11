7dde87d45255ec22158b6d8986c7becfea40551b
Krishna Kumar <krishnalkr1210@gmail.com>
2022-01-11 20:53:25 +0530

    hw completed

---
+++ c programs/circumference.c
@@ -0,0 +1,15 @@
+//completed
+//Circumference of Circle when radius is given
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    float r,circumference;
+    printf("Enter the radius of the Circle ");
+    scanf("%f",&r);
+    circumference=3.142857142857143*2*r;
+    printf("Circumference of the Circle is " "%.2f",circumference);
+    getchar();
+
+
+}
