5f546ce17898e26192c944c65ef7913bd51adbe9
Krishna Kumar <krishnalkr1210@gmail.com>
2022-01-11 20:16:19 +0530

    area of traiangle

---
+++ c programs/areaoftriangle.c
@@ -0,0 +1,15 @@
+//Completed
+//Area of a right angled traiangle when base and height is given
+
+#include<stdio.h>
+void main()
+{
+    
+    float ar,a,b,h;
+    printf("Enter base of the triangle");
+    scanf("%f",&b);
+    printf("Enter height of the triangle");
+    scanf("%f",&h);
+    ar=0.5*b*h;
+    printf("%.2f",ar);
+}
