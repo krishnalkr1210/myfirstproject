7c892407fe2ac517e1c0e198c49e35a9dae4325d
Krishna Kumar <krishnalkr1210@gmail.com>
2022-03-28 16:40:00 +0530

    update

--- test/test.c
+++ test/test.c
@@ -1,6 +1,21 @@
-#include <stdio.h> 
-int main() 
+
+
+#include<stdio.h>
+void sum(int a , int b); //Function Prototype
+
+int main(int argc, char const *argv[])
 {
-    int  x=12;
-    printf("%d %d",++x,x);
+    int x,y;
+    printf("Enter Two Numbers ");
+    scanf("%d%d",&y,&x);
+    // printf("Enter Second Number ");
+    // scanf("%d",&y);
+
+    sum(x,y); //Function call
+    return 0;
+}
+
+// Function Definition
+void sum(int a , int b){
+    printf("Sum of %d and %d is %d",a,b,a+b);
 }
---
+++ test/test2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a,b;
+    for ( i = 0; i < count; i++)
+    {
+        /* code */
+    }
+    
+    return 0;
+}
